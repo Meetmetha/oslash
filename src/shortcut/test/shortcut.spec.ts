f323d9ca8df3236b63866ab0608f8e4a874689cf
Mitesh Metha <miteshmehta2018@gmail.com>
2021-12-09 13:02:54 +0530

    testAdd but versionConflict exists

--- src/shortcut/test/shortcut.spec.ts
+++ src/shortcut/test/shortcut.spec.ts
@@ -1,3 +1,4 @@
+import { UnprocessableEntityException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShortcutController } from '../controller/controller';
 import { ShortcutService } from '../services/service';
@@ -18,12 +19,37 @@ describe('ShortcutController', () => {
     shortcutService = module.get<ShortcutService>(ShortcutService);
   });
 
-  it('ShortcutService - should be called', () => {
+  it('ShortcutService:getting UserShortcut - should be called', () => {
     const user = {userid:'user-id-from-jwt'};
     const inputs = {
-      'search': 'searchQuery',
+      'search': 'searchQuery', //Search Query to Search
     };
-    expect(shortcutService.getUserShortcut(user,inputs)).toHaveBeenCalled();
+    const returnHits = {
+      description: "This is Hira",
+      tags: ['jira'],
+      _id: 'mongodb-objectid',
+      user: 'user-id-from-jwt',
+      shortlink: 'jira',
+      url: 'https://miteshmetha.com',
+      createdAt: 'date',
+      uodatedAt: 'date',
+      __V: 0
+    }
+    return shortcutService.getUserShortcut(user,inputs).then((returnedResult) => {
+      expect(returnedResult).toBe(returnHits);
+    });
+  });
+
+  it('should throw empty data', () => {
+    const user = {userid:'user-id-from-jwt'};
+    const inputs = {
+      'search': '', //Empty Search Term
+    };
+    return shortcutService.getUserShortcut(user,inputs).then((returnedResult) => {
+      expect(returnedResult).rejects.toThrow(
+        UnprocessableEntityException,
+      );
+    });
   });
 
 
